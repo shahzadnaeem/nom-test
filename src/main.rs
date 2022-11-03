893ddcd32d2fe09251a20f48685d9e3cd107a3aa
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-11-03 17:57:38 +0000

    The hex color example with additions

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+extern crate nom;
+
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_while_m_n},
+    combinator::map_res,
+    sequence::tuple,
+    IResult,
+};
+
+#[derive(Debug, PartialEq)]
+pub struct Color {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
+fn from_hex1(input: &str) -> Result<u8, std::num::ParseIntError> {
+    match u8::from_str_radix(input, 16) {
+        Ok(num) => Ok(num + 16 * num),
+        err => err,
+    }
+}
+
+fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
+    u8::from_str_radix(input, 16)
+}
+
+fn is_hex_digit(c: char) -> bool {
+    c.is_digit(16)
+}
+
+fn hex_primary1(input: &str) -> IResult<&str, u8> {
+    map_res(take_while_m_n(1, 1, is_hex_digit), from_hex1)(input)
+}
+
+fn hex_primary2(input: &str) -> IResult<&str, u8> {
+    map_res(take_while_m_n(2, 2, is_hex_digit), from_hex)(input)
+}
+
+fn hex_color6(input: &str) -> IResult<&str, Color> {
+    let (input, _) = tag("#")(input)?;
+    let (input, (red, green, blue)) = tuple((hex_primary2, hex_primary2, hex_primary2))(input)?;
+
+    Ok((input, Color { red, green, blue }))
+}
+
+fn hex_color3(input: &str) -> IResult<&str, Color> {
+    let (input, _) = tag("#")(input)?;
+    let (input, (red, green, blue)) = tuple((hex_primary1, hex_primary1, hex_primary1))(input)?;
+
+    Ok((input, Color { red, green, blue }))
+}
+
+fn hex_color(input: &str) -> IResult<&str, Color> {
+    alt((hex_color6, hex_color3))(input)
+}
+
+fn main() {
+    const INPUT: &str = "#555555";
+    let res = hex_color(INPUT);
+
+    match res {
+        Ok(col) => print!("{} => {:?}\n", INPUT, col.1),
+        Err(err) => print!("ERROR: {:?}", err),
+    };
+
+    const INPUT2: &str = "#FFF";
+    let res = hex_color(INPUT2);
+
+    match res {
+        Ok(col) => print!("{} => {:?}\n", INPUT2, col.1),
+        Err(err) => print!("ERROR: {:?}", err),
+    };
+}
+
+#[test]
+fn parse_color() {
+    assert_eq!(
+        hex_color("#2F14DF"),
+        Ok((
+            "",
+            Color {
+                red: 47,
+                green: 20,
+                blue: 223,
+            }
+        ))
+    );
+}
+
+#[test]
+fn parse_color_3_digits() {
+    assert_eq!(
+        hex_color("#FFF"),
+        Ok((
+            "",
+            Color {
+                red: 255,
+                green: 255,
+                blue: 255,
+            }
+        ))
+    );
+}
